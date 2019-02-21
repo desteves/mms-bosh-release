2860024accc1e37d26ead97cbce61aeb8455bc20
dianaesteves <desteves@users.noreply.github.com>
2019-02-21 10:42:25 -0800

    Update run.sh

--- jobs/global_owner/templates/run.sh
+++ jobs/global_owner/templates/run.sh
@@ -7,20 +7,23 @@ FQDN=<%= properties.mms.fqdn %>
 PORT=<%= properties.mms.port %>
 MMSG=<%= properties.mms.org %>
 
-echo "Waiting up to 2 minutes for HTTP(S) to load..."
+echo "Waiting up to 4 minutes for HTTP(S) to load..."
 ## curl 7.35.0 doesnt have the --retry-connrefused option :/, using without it
-curl 127.0.0.1:${PORT} -sf --retry 4 --retry-delay 30
+curl ${FQDN}:${PORT} -sf --retry 8 --retry-delay 30
 if [ $? -ne 0 ]; then
-    echo "Failed to register Global Owner. Check Logs and Try Again"
-    exit 1
+    echo "Failed get response from MMS to register Global Owner, trying anyways..."
 fi
 
 echo "Registering admin user (username: $USER, password: $PASS)"
+
 ###  Your password must be at least eight (8) characters long and contain at least one letter, one digit and one special character.
-curl -X POST \
+
+rp=$( \
+curl -X POST   \
 http://127.0.0.1:${PORT}/user/registerCall \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
-  -d "{\"username\":\"$USER\",\"password\":\"$PASS\",\"firstName\":\"Global\",\"lastName\":\"Owner\",\"newGroup\":true,\"jobResponsibility\":\"\",\"response\":\"\"}" 
+  -d "{\"username\":\"$USER\",\"password\":\"$PASS\",\"firstName\":\"Global\",\"lastName\":\"Owner\",\"newGroup\":true,\"jobResponsibility\":\"\",\"response\":\"\"}" )
+e=$(echo $rp | grep  "\"errorCode\":\"NONE\"")
 
-exit 
+if [ -z $e ]; then exit 0; else exit 1; fi
