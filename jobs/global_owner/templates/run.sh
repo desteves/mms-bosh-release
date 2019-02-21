499e0df173f35cc7c8e88918113bf8f9b27d6894
dianaesteves <desteves@users.noreply.github.com>
2019-02-21 00:58:55 -0800

    Update run.sh

--- jobs/global_owner/templates/run.sh
+++ jobs/global_owner/templates/run.sh
@@ -9,7 +9,7 @@ MMSG=<%= properties.mms.org %>
 
 echo "Waiting up to 2 minutes for HTTP(S) to load..."
 ## curl 7.35.0 doesnt have the --retry-connrefused option :/, using without it
-curl localhost:${PORT} -sf --retry 4 --retry-delay 30
+curl 127.0.0.1:${PORT} -sf --retry 4 --retry-delay 30
 if [ $? -ne 0 ]; then
     echo "Failed to register Global Owner. Check Logs and Try Again"
     exit 1
@@ -18,7 +18,7 @@ fi
 echo "Registering admin user (username: $USER, password: $PASS)"
 ###  Your password must be at least eight (8) characters long and contain at least one letter, one digit and one special character.
 curl -X POST \
-http://localhost:${PORT}/user/registerCall \
+http://127.0.0.1:${PORT}/user/registerCall \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -d "{\"username\":\"$USER\",\"password\":\"$PASS\",\"firstName\":\"Global\",\"lastName\":\"Owner\",\"newGroup\":true,\"jobResponsibility\":\"\",\"response\":\"\"}" 
